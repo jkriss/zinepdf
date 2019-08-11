fcd130fea223c12fcbcfcae9b5ed6b5cef130f5d
Jesse Kriss <jesse@jklabs.net>
2019-08-10 22:47:49 -0700

    Getting rotation working

--- zinepdf.py
+++ zinepdf.py
@@ -16,17 +16,16 @@ m = 5 # margin
 # each page should be 2.75 x 4.25
 # scaled up to roughly 8.5 x 11, that's 8.5 x 13.13 (just shy of legal size)
 
-#zine.mergeRotatedScaledTranslatedPage(input1.getPage(0), 180, scale, scale*pw*2, pw/2)
-zine.mergeRotatedScaledTranslatedPage(input.getPage(1), 0, scale, (3*pw/4)+m, (ph/2)-m)
-zine.mergeRotatedScaledTranslatedPage(input.getPage(2), 0, scale, 2*pw/4, (ph/2)-m)
-zine.mergeRotatedScaledTranslatedPage(input.getPage(3), 0, scale, pw/4, (ph/2)-m)
-zine.mergeRotatedScaledTranslatedPage(input.getPage(4), 0, scale, 0, (ph/2)-m)
+zine.mergeRotatedScaledTranslatedPage(input.getPage(1), 180, scale, pw-m, ph-m)
+zine.mergeRotatedScaledTranslatedPage(input.getPage(2), 180, scale, (3*pw/4)-m, ph-m)
+zine.mergeRotatedScaledTranslatedPage(input.getPage(3), 180, scale, (2*pw/4)-m, ph-m)
+zine.mergeRotatedScaledTranslatedPage(input.getPage(4), 180, scale, (pw/4)-m, ph-m)
 
-zine.mergeRotatedScaledTranslatedPage(input.getPage(0), 0, scale, 3*pw/4, -m)
-zine.mergeRotatedScaledTranslatedPage(input.getPage(5), 0, scale, 0, -m)
-zine.mergeRotatedScaledTranslatedPage(input.getPage(6), 0, scale, pw/4, -m)
+zine.mergeRotatedScaledTranslatedPage(input.getPage(0), 0, scale, (3*pw/4)+m, -m)
+zine.mergeRotatedScaledTranslatedPage(input.getPage(5), 0, scale, m, -m)
+zine.mergeRotatedScaledTranslatedPage(input.getPage(6), 0, scale, (pw/4)+m, -m)
 if pageCount > 7:
-    zine.mergeRotatedScaledTranslatedPage(input.getPage(7), 0, scale, 2*pw/4, -m)
+    zine.mergeRotatedScaledTranslatedPage(input.getPage(7), 0, scale, (2*pw/4)+m, -m)
 
 # finally, write "output" to document-output.pdf
 outputStream = open("zine.pdf", "wb")
